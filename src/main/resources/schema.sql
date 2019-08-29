8b7b5a110010b4ac956733d9ea3b8f32da7ea8eb
wanghui <793147654@qq.com>
2019-08-29 19:59:17 +0800

    添加SQL语句

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,30 +1,36 @@
 CREATE TABLE employee (
   id INTEGER PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
-  age   int(4) NOT NULL
+  age   INTEGER NOT NULL,
+  address VARCHAR(255),
+  username VARCHAR(255),
+  password VARCHAR(255),
+  companyId INTEGER,
+  tagId INTEGER,
+  roleId INTEGER
 );
 
 
 CREATE TABLE company (
   id INTEGER PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255)
 );
 
 CREATE TABLE application (
   id INTEGER PRIMARY KEY,
   name VARCHAR(255),
-  greeting_message VARCHAR(255),
+  greeting_message VARCHAR(255)
 );
 
 CREATE TABLE role (
   id INTEGER PRIMARY KEY,
   name VARCHAR(255),
-  auth VARCHAR(255),
+  auth VARCHAR(255)
 );
 
 CREATE TABLE tag (
   id INTEGER PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255)
 );
 
 

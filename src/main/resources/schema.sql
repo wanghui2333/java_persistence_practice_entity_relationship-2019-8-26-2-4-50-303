f0dd152a2e0363653c4821757c256c4400aefe83
wanghui <793147654@qq.com>
2019-08-29 19:57:10 +0800

    添加SQL语句

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,28 @@ CREATE TABLE employee (
   name VARCHAR(64) NOT NULL,
   age   int(4) NOT NULL
 );
+
+
+CREATE TABLE company (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+);
+
+CREATE TABLE application (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  greeting_message VARCHAR(255),
+);
+
+CREATE TABLE role (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  auth VARCHAR(255),
+);
+
+CREATE TABLE tag (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+);
+
+
